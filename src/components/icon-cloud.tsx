2c6fb4fac115e441ff379ce625c173ef1e964d4f
Giordano Borsato <borsatogiordano@gmail.com>
2025-09-26 15:36:09 -0300

    feat: first steps into portfolio

---
+++ src/components/icon-cloud.tsx
@@ -0,0 +1,38 @@
+import { IconCloud } from "./ui/icon-cloud"
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "java",
+  "react",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "postgresql",
+  "nginx",
+  "vercel",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "visualstudiocode",
+  "figma",
+  "photoshop"
+]
+
+export function IconCloudComponent() {
+  const images = slugs.map(
+    (slug) => `https://cdn.simpleicons.org/${slug}/${slug}`
+  )
+
+  return (
+    <div className="relative flex size-full items-center justify-center overflow-hidden ">
+      <IconCloud images={images} />
+    </div>
+  )
+}
